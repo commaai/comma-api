6dc346876c16aa4ac48884c1261b2bb29720cbb0
Andy Haden <andy@comma.ai>
2019-06-14 02:24:30 -0700

    get index file urls from video api

--- dist/video.js
+++ dist/video.js
@@ -10,6 +10,10 @@ var _slicedToArray3 = _interopRequireDefault(_slicedToArray2);
 
 exports.default = videoApi;
 
+var _urlJoin = require('url-join');
+
+var _urlJoin2 = _interopRequireDefault(_urlJoin);
+
 var _configRequestPromise = require('./config-request-promise');
 
 var _configRequestPromise2 = _interopRequireDefault(_configRequestPromise);
@@ -23,22 +27,29 @@ function videoApi(routeSigUrl, videoServerHost) {
     videoServerHost = _config.VIDEO_URL_ROOT;
   }
 
-  var _routeSigUrl$split$sl = routeSigUrl.split().slice(5, 7),
+  var _routeSigUrl$split$sl = routeSigUrl.split('/').slice(5, 7),
       _routeSigUrl$split$sl2 = (0, _slicedToArray3.default)(_routeSigUrl$split$sl, 2),
       dongleId = _routeSigUrl$split$sl2[0],
       routeSignature = _routeSigUrl$split$sl2[1];
 
   var request = (0, _configRequestPromise2.default)();
+  var baseUrl = videoServerHost + '/hls/' + dongleId + '/' + routeSignature + '/';
   request.configure({
-    baseUrl: videoServerHost + '/hls/' + dongleId + '/' + routeSignature + '/',
+    baseUrl: baseUrl,
     parse: null
   });
 
   return {
+    getRearCameraStreamIndexUrl: function getRearCameraStreamIndexUrl() {
+      return (0, _urlJoin2.default)(baseUrl, 'index.m3u8');
+    },
+    getFrontCameraStreamIndexUrl: function getFrontCameraStreamIndexUrl() {
+      return (0, _urlJoin2.default)(baseUrl, 'dcamera/index.m3u8');
+    },
     getRearCameraStreamIndex: function getRearCameraStreamIndex() {
       return request.get('index.m3u8');
     },
-    getFrontCameraStreamIndex: function getFrontCameraStreamIndex() {
+    getFrontCameraStreamIndexPath: function getFrontCameraStreamIndexPath() {
       return request.get('dcamera/index.m3u8');
     }
   };
--- src/video.js
+++ src/video.js
@@ -1,3 +1,4 @@
+import urlJoin from 'url-join';
 import ConfigRequestPromise from './config-request-promise';
 import { VIDEO_URL_ROOT } from './config';
 
@@ -5,19 +6,26 @@ export default function videoApi(routeSigUrl, videoServerHost) {
   if (!videoServerHost) {
     videoServerHost = VIDEO_URL_ROOT;
   }
-  let [dongleId, routeSignature] = routeSigUrl.split().slice(5,7);
+  let [dongleId, routeSignature] = routeSigUrl.split('/').slice(5,7);
 
   const request = ConfigRequestPromise();
+  const baseUrl = videoServerHost + '/hls/' + dongleId + '/' + routeSignature + '/'
   request.configure({
-    baseUrl: videoServerHost + '/hls/' + dongleId + '/' + routeSignature + '/',
+    baseUrl: baseUrl,
     parse: null,
   })
 
   return {
+    getRearCameraStreamIndexUrl: function() {
+      return urlJoin(baseUrl, 'index.m3u8');
+    },
+    getFrontCameraStreamIndexUrl: function() {
+      return urlJoin(baseUrl, 'dcamera/index.m3u8');
+    },
     getRearCameraStreamIndex: function() {
       return request.get('index.m3u8');
     },
-    getFrontCameraStreamIndex: function() {
+    getFrontCameraStreamIndexPath: function() {
       return request.get('dcamera/index.m3u8');
     },
   }
