8e25cb41bc0b465f293a7d90c6e606ccefcd8761
Andy Haden <andy@comma.ai>
2019-06-17 02:08:48 -0700

    use v2/auth/

--- dist/auth.js
+++ dist/auth.js
@@ -3,11 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.refreshCommaUser = exports.refreshAccessToken = undefined;
-
-var _stringify = require('babel-runtime/core-js/json/stringify');
-
-var _stringify2 = _interopRequireDefault(_stringify);
+exports.refreshAccessToken = undefined;
 
 var _regenerator = require('babel-runtime/regenerator');
 
@@ -18,14 +14,14 @@ var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
 var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
 
 var refreshAccessToken = exports.refreshAccessToken = function () {
-  var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(idToken) {
+  var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(code) {
     var authResponseText, resp;
     return _regenerator2.default.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
             _context.next = 2;
-            return request.postForm(AUTH_ENDPOINT, { id_token: idToken });
+            return request.postForm('v2/auth/', { code: code });
 
           case 2:
             authResponseText = _context.sent;
@@ -71,45 +67,6 @@ var refreshAccessToken = exports.refreshAccessToken = function () {
   };
 }();
 
-var refreshCommaUser = exports.refreshCommaUser = function () {
-  var _ref2 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(accessToken) {
-    var commaUser;
-    return _regenerator2.default.wrap(function _callee2$(_context2) {
-      while (1) {
-        switch (_context2.prev = _context2.next) {
-          case 0:
-            _context2.next = 2;
-            return request.get(USER_ENDPOINT);
-
-          case 2:
-            commaUser = _context2.sent;
-
-            if (!(commaUser.success !== false)) {
-              _context2.next = 8;
-              break;
-            }
-
-            commaUser.accessToken = accessToken;
-            return _context2.abrupt('return', commaUser);
-
-          case 8:
-            throw new Error('Failed to fetch comma user: ' + (0, _stringify2.default)(commaUser));
-
-          case 9:
-          case 'end':
-            return _context2.stop();
-        }
-      }
-    }, _callee2, this);
-  }));
-
-  return function refreshCommaUser(_x2) {
-    return _ref2.apply(this, arguments);
-  };
-}();
-
-exports.commaTokenExchange = commaTokenExchange;
-
 var _request = require('./request');
 
 var request = _interopRequireWildcard(_request);
@@ -117,13 +74,3 @@ var request = _interopRequireWildcard(_request);
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var AUTH_ENDPOINT = 'v1/auth/';
-var USER_ENDPOINT = 'v1/me/';
-
-function commaTokenExchange(accessToken, idToken) {
-  return request.postForm("auth/", {
-    access_token: accessToken,
-    id_token: idToken
-  });
-}
--- src/auth.js
+++ src/auth.js
@@ -1,10 +1,7 @@
 import * as request from './request';
 
-const AUTH_ENDPOINT = 'v1/auth/';
-const USER_ENDPOINT = 'v1/me/';
-
-export async function refreshAccessToken(idToken) {
-  const authResponseText = await request.postForm(AUTH_ENDPOINT, { id_token: idToken });
+export async function refreshAccessToken(code) {
+  const authResponseText = await request.postForm('v2/auth/', { code });
   const resp = JSON.parse(authResponseText);
   if (resp !== undefined) {
     if (resp.access_token != null) {
@@ -17,19 +14,3 @@ export async function refreshAccessToken(idToken) {
   throw new Error('Could not exchange idToken for access token: response undefined.');
 }
 
-export async function refreshCommaUser(accessToken) {
-  const commaUser = await request.get(USER_ENDPOINT);
-  if (commaUser.success !== false) {
-    commaUser.accessToken = accessToken;
-    return commaUser;
-  } else {
-    throw new Error('Failed to fetch comma user: ' + JSON.stringify(commaUser));
-  }
-}
-
-export function commaTokenExchange(accessToken, idToken) {
-  return request.postForm("auth/", {
-    access_token: accessToken,
-    id_token: idToken
-  });
-}
