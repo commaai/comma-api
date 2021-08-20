6e6433a048859a8cf89eaa61f9cc540cd8026d54
Joost Wooning <jwooning@gmail.com>
2021-08-20 11:35:27 +0200

    configurable config

--- dist/config.js
+++ dist/config.js
@@ -3,10 +3,22 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var COMMA_URL_ROOT = exports.COMMA_URL_ROOT = 'https://api.commadotai.com/';
-var ATHENA_URL_ROOT = exports.ATHENA_URL_ROOT = 'https://athena.comma.ai/';
-var BILLING_URL_ROOT = exports.BILLING_URL_ROOT = 'https://billing.comma.ai/';
-var VIDEO_HOST = exports.VIDEO_HOST = process.env.COMMA_VIDEO_CDN;
+var COMMA_URL_ROOT = exports.COMMA_URL_ROOT = window.COMMA_URL_ROOT;
+if (!COMMA_URL_ROOT) {
+  exports.COMMA_URL_ROOT = COMMA_URL_ROOT = 'https://api.commadotai.com/';
+}
+
+var ATHENA_URL_ROOT = exports.ATHENA_URL_ROOT = window.ATHENA_URL_ROOT;
+if (!ATHENA_URL_ROOT) {
+  exports.ATHENA_URL_ROOT = ATHENA_URL_ROOT = 'https://athena.comma.ai/';
+}
+
+var BILLING_URL_ROOT = exports.BILLING_URL_ROOT = window.BILLING_URL_ROOT;
+if (!BILLING_URL_ROOT) {
+  exports.BILLING_URL_ROOT = BILLING_URL_ROOT = 'https://billing.comma.ai/';
+}
+
+var VIDEO_HOST = exports.VIDEO_HOST = window.COMMA_VIDEO_CDN;
 if (!VIDEO_HOST) {
   exports.VIDEO_HOST = VIDEO_HOST = 'https://my-comma-video.azureedge.net';
 }
--- src/config.js
+++ src/config.js
@@ -1,7 +1,19 @@
-export const COMMA_URL_ROOT = 'https://api.commadotai.com/';
-export const ATHENA_URL_ROOT = 'https://athena.comma.ai/';
-export const BILLING_URL_ROOT = 'https://billing.comma.ai/';
-export let VIDEO_HOST = process.env.COMMA_VIDEO_CDN;
+export let COMMA_URL_ROOT = window.COMMA_URL_ROOT;
+if (!COMMA_URL_ROOT) {
+  COMMA_URL_ROOT = 'https://api.commadotai.com/';
+}
+
+export let ATHENA_URL_ROOT = window.ATHENA_URL_ROOT;
+if (!ATHENA_URL_ROOT) {
+  ATHENA_URL_ROOT = 'https://athena.comma.ai/';
+}
+
+export let BILLING_URL_ROOT = window.BILLING_URL_ROOT;
+if (!BILLING_URL_ROOT) {
+  BILLING_URL_ROOT = 'https://billing.comma.ai/';
+}
+
+export let VIDEO_HOST = window.COMMA_VIDEO_CDN;
 if (!VIDEO_HOST) {
   VIDEO_HOST = 'https://my-comma-video.azureedge.net';
 }
