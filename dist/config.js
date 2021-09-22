61c98530be0c0bc4c6a31bf79705f3ee3414350a
Joost Wooning <jwooning@gmail.com>
2021-09-22 17:43:18 +0200

    check window for workers

--- dist/config.js
+++ dist/config.js
@@ -3,22 +3,22 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var COMMA_URL_ROOT = exports.COMMA_URL_ROOT = window.COMMA_URL_ROOT;
-if (!COMMA_URL_ROOT) {
-  exports.COMMA_URL_ROOT = COMMA_URL_ROOT = 'https://api.commadotai.com/';
+var COMMA_URL_ROOT = exports.COMMA_URL_ROOT = 'https://api.comma.ai/';
+if (typeof window !== 'undefined' && window.COMMA_URL_ROOT) {
+  exports.COMMA_URL_ROOT = COMMA_URL_ROOT = window.COMMA_URL_ROOT;
 }
 
-var ATHENA_URL_ROOT = exports.ATHENA_URL_ROOT = window.ATHENA_URL_ROOT;
-if (!ATHENA_URL_ROOT) {
-  exports.ATHENA_URL_ROOT = ATHENA_URL_ROOT = 'https://athena.comma.ai/';
+var ATHENA_URL_ROOT = exports.ATHENA_URL_ROOT = 'https://athena.comma.ai/';
+if (typeof window !== 'undefined' && window.ATHENA_URL_ROOT) {
+  exports.ATHENA_URL_ROOT = ATHENA_URL_ROOT = window.ATHENA_URL_ROOT;
 }
 
-var BILLING_URL_ROOT = exports.BILLING_URL_ROOT = window.BILLING_URL_ROOT;
-if (!BILLING_URL_ROOT) {
-  exports.BILLING_URL_ROOT = BILLING_URL_ROOT = 'https://billing.comma.ai/';
+var BILLING_URL_ROOT = exports.BILLING_URL_ROOT = 'https://billing.comma.ai/';
+if (typeof window !== 'undefined' && window.BILLING_URL_ROOT) {
+  exports.BILLING_URL_ROOT = BILLING_URL_ROOT = window.BILLING_URL_ROOT;
 }
 
-var VIDEO_HOST = exports.VIDEO_HOST = window.COMMA_VIDEO_CDN;
-if (!VIDEO_HOST) {
-  exports.VIDEO_HOST = VIDEO_HOST = 'https://my-comma-video.azureedge.net';
+var VIDEO_HOST = exports.VIDEO_HOST = 'https://my-comma-video.azureedge.net';
+if (typeof window !== 'undefined' && window.VIDEO_HOST) {
+  exports.VIDEO_HOST = VIDEO_HOST = window.VIDEO_HOST;
 }
--- src/config.js
+++ src/config.js
@@ -1,19 +1,19 @@
-export let COMMA_URL_ROOT = window.COMMA_URL_ROOT;
-if (!COMMA_URL_ROOT) {
-  COMMA_URL_ROOT = 'https://api.commadotai.com/';
+export let COMMA_URL_ROOT = 'https://api.comma.ai/';
+if (typeof window !== 'undefined' && window.COMMA_URL_ROOT) {
+  COMMA_URL_ROOT = window.COMMA_URL_ROOT;
 }
 
-export let ATHENA_URL_ROOT = window.ATHENA_URL_ROOT;
-if (!ATHENA_URL_ROOT) {
-  ATHENA_URL_ROOT = 'https://athena.comma.ai/';
+export let ATHENA_URL_ROOT = 'https://athena.comma.ai/';
+if (typeof window !== 'undefined' && window.ATHENA_URL_ROOT) {
+  ATHENA_URL_ROOT = window.ATHENA_URL_ROOT;
 }
 
-export let BILLING_URL_ROOT = window.BILLING_URL_ROOT;
-if (!BILLING_URL_ROOT) {
-  BILLING_URL_ROOT = 'https://billing.comma.ai/';
+export let BILLING_URL_ROOT = 'https://billing.comma.ai/';
+if (typeof window !== 'undefined' && window.BILLING_URL_ROOT) {
+  BILLING_URL_ROOT = window.BILLING_URL_ROOT;
 }
 
-export let VIDEO_HOST = window.COMMA_VIDEO_CDN;
-if (!VIDEO_HOST) {
-  VIDEO_HOST = 'https://my-comma-video.azureedge.net';
+export let VIDEO_HOST = 'https://my-comma-video.azureedge.net';
+if (typeof window !== 'undefined' && window.VIDEO_HOST) {
+  VIDEO_HOST = window.VIDEO_HOST;
 }
