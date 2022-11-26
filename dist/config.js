a08528c7f6b35ee45d382e5b31a6b8fd5a0041a9
Cameron Clough <cameronjclough@gmail.com>
2022-11-26 15:01:47 -0800

    remove dist

--- dist/config.js
+++
@@ -1,6 +0,0 @@
-var _window, _window2, _window3;
-export default {
-  COMMA_URL_ROOT: ((_window = window) === null || _window === void 0 ? void 0 : _window.COMMA_URL_ROOT) || 'https://api.comma.ai/',
-  ATHENA_URL_ROOT: ((_window2 = window) === null || _window2 === void 0 ? void 0 : _window2.ATHENA_URL_ROOT) || 'https://athena.comma.ai/',
-  BILLING_URL_ROOT: ((_window3 = window) === null || _window3 === void 0 ? void 0 : _window3.BILLING_URL_ROOT) || 'https://billing.comma.ai/'
-};
--- dist/request.js
+++
@@ -1,3 +0,0 @@
-import Config from './config';
-import ConfigRequest from './instance';
-export default new ConfigRequest(Config.COMMA_URL_ROOT);
